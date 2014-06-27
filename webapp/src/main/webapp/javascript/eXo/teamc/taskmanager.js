3ff2d2541848a97d9cd528359c4fc31f9c512bca
anhvt <anhvta@yahoo.com>
2014-06-27 17:58:46 +0700

    apply action into layout

--- webapp/src/main/webapp/javascript/eXo/teamc/taskmanager.js
+++ webapp/src/main/webapp/javascript/eXo/teamc/taskmanager.js
@@ -1,4 +1,4 @@
-(function() {
+(function(gj) {
 	function TaskManager() {
 		
 	};
@@ -9,26 +9,97 @@
 			this.getProjects();
 	};
 	TaskManager.prototype.showProjectCreationForm = function(){
-		alert('show popup form');	
+		this.showPopupContainer('uiPopupProjectCreationForm');
 	};
-	TaskManager.prototype.ajaxCommonRequest = function(action){
-		alert('send request to'+action);
+	TaskManager.prototype.ajaxCommonRequest = function(data){
+
+		var url = this.getRestByAction(data.action);
+		gj.ajax({
+  			dataType: "json",
+  			url: url,
+  			data: data,
+  			success: function(){
+
+  			}
+		});
+		this.closePopupContainer();
 	};
+	TaskManager.prototype.getRestByAction = function(action){
+		var rest = '/rest/taskmanager/';
+		if(action == 'createProject'){
+			rest +='createProject';	
+		}
+		return rest;
+	}
 	TaskManager.prototype.getProjects = function(){
-		alert('get project json');
+		console.info('get project json');
 		this.showProjects('');
 	};
 	TaskManager.prototype.showProjects = function (projects){
-		alert('show projects in select');
+		
+		console.info('show projects in select');
 	};
+	TaskManager.prototype.getInputVal = function(inputName){
+		return gj('.LightBoxContent input[name='+inputName+']').val();
+	}
 	TaskManager.prototype.doCreateProject = function(){
-		alert('create project');
-		this.ajaxCommonRequest('createProject');
-	};
-	TaskManager.prototype.validProjectCreationForm = function (){
-		alert('validate form');
-	};
-	var TaskManager = new TaskManager();
-	return TaskManager;
-})();
 
+		var data = {
+			'action':'createProject',
+			'name':this.getInputVal('displayName'),
+			'description':this.getInputVal('description'),
+			'memberIds':this.getInputVal('displayMember'),	
+			'managerIds':this.getInputVal('displayManager')		
+	};
+
+		this.ajaxCommonRequest(data);
+	};
+	TaskManager.prototype.closePopupContainer = function(){
+		gj(".LightBoxContent").html('');
+		var lightboxContainer = gj(".LightBoxContainer"); 
+		gj(lightboxContainer).hide();
+	}
+	TaskManager.prototype.validProjectCreationForm = function (){
+		console.log('validate form');
+	};
+	TaskManager.prototype.showPopupContainer = function(contentid){
+		var childDOM = gj("#"+contentid);
+		var htmlContent = childDOM.html();
+		var lightboxContainer = gj(".LightBoxContainer"); 
+	
+		gj(".LightBoxContent").html(htmlContent);
+		if(gj("#exo-mask").length == 0) {
+			var mask = gj("<div />", {
+				id : "exo-mask",
+				style : "opacity:0.8;",
+				click : function(){
+					gj(".LightBoxContent").html('');
+					gj(lightboxContainer).hide();
+					gj(this).remove();
+					gj("body").css("overflow", "auto");
+				} 	
+			});
+		}
+		gj(lightboxContainer).before(mask);
+
+		gj("a.BtnClose").click(function(){
+			gj(".LightBoxContent").html('');
+			gj(lightboxContainer).hide();
+			gj("#exo-mask").remove();	
+			gj("body").css("overflow", "visible");
+		});
+
+		var top = (gj(window).height() - lightboxContainer.outerHeight())/2 ;
+		top += gj(window).scrollTop() || 0;
+		
+		var left = (gj(window).width() - gj(lightboxContainer).width())/4;
+		lightboxContainer.css("top", top);
+		lightboxContainer.css("left", left);
+
+		gj("body").css("overflow", "hidden");
+	
+		lightboxContainer.show();		
+	}
+	window.TaskManager = new TaskManager();
+	return window.TaskManager;
+})(gj);
